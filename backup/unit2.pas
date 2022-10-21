2af114588f1b45ed8a874a156adf8c7290f1eff5
AirMagicTY <airmagicty@gmail.com>
2022-10-21 16:40:04 +0300

    Initial commit

---
+++ backup/unit2.pas
@@ -0,0 +1,149 @@
+unit Unit2;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  Buttons, PopupNotifier, ValEdit, LazHelpHTML, ComCtrls, RichMemo, RichView,
+  RVStyle, PtblRV, RTTICtrls, SynEdit, SynExportHTML, SynCompletion,
+  SynPluginSyncroEdit, IpHtml, JLabeledIntegerEdit, Types;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    BitBtn1: TBitBtn;
+    Button1: TButton;
+    Button2: TButton;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    RichMemo1: TRichMemo;
+    Timer1: TTimer;
+    procedure BitBtn1Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form2: TForm2;
+  stime: integer;
+  res_str: string;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+uses Unit1;
+
+{ TForm2 }
+
+// Закрыть оба окна при закрытии Form2
+procedure TForm2.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  Form1.Close;
+end;
+
+procedure TForm2.FormCreate(Sender: TObject);
+var rs : TResourceStream;
+begin
+  // загрузка примера отчета из сурса
+  rs := TResourceStream.Create(HInstance, 'TEST', PChar(RT_RCDATA));
+  Form2.RichMemo1.LoadRichText(rs);
+  FreeAndNil(rs);
+end;
+
+// запуск ожидания
+procedure TForm2.FormActivate(Sender: TObject);
+begin
+  // обозначить экзамен или тест
+  if Form1.RadioButton1.Checked = true then
+    stime := 2; // 10-1
+  if Form1.RadioButton2.Checked = true then
+    stime := 29; // 30-1
+  Form2.Timer1.Enabled := true;
+end;
+
+// по таймеру открыть кнопку отчета
+procedure TForm2.Timer1Timer(Sender: TObject);
+begin
+  // пока идет таймер
+  if stime = 0 then
+  begin
+    // таймер кончился
+    Form2.Timer1.Enabled := false;
+    Form2.Label3.Visible := false;
+    Form2.Button1.Visible := true;
+    Form2.Button1.SetFocus;
+  end
+  else
+  begin
+    // окончание
+    if (stime >= 11) and (stime <= 14) then
+      res_str := 'секунд'
+    else case stime mod 10 of
+      1: res_str := 'секунду';
+      2..4: res_str := 'секунды';
+      else res_str := 'секунд';
+    end;
+    Form2.Label3.Caption := 'Подождите ' + IntToStr(stime) + ' ' + res_str;
+
+    // таймер идет
+    stime := stime - 1;
+  end;
+end;
+
+// показать отчет
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+  // скрыть старое
+  Form2.Label1.Visible := false;
+  Form2.Label2.Visible := false;
+  Form2.Button1.Visible := false;
+
+  // открыть новое
+  Form2.Button2.Visible := true;
+  Form2.BitBtn1.Visible := true;
+  Form2.RichMemo1.Visible := true;
+
+  // выделить синим Печать
+  Form2.Button2.SetFocus;
+end;
+
+procedure TForm2.Button2Click(Sender: TObject);
+var
+  srtf: TFileStream;
+begin
+  srtf := nil;
+  with TOpenDialog.Create(self) do
+  try
+    Filter:='RTF документы |*.rtf|'+'Все файлы |*.*|';
+  if Execute then
+  begin
+    srtf := TFileStream.Create(Utf8ToAnsi(FileName), fmOpenRead or fmShareDenyNone);
+    RichMemo1.LoadRichText(srtf);
+  end;
+  finally
+    Free;
+  end;
+end;
+
+procedure TForm2.BitBtn1Click(Sender: TObject);
+begin
+  // Тут я тестировал разную фигню, кнопка для отладки крч
+end;
+
+end.
+
---
+++ unit1.pas
@@ -0,0 +1,206 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons,
+  LazUTF8, Process;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    BitBtn1: TBitBtn;
+    Button1: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Edit5: TEdit;
+    Label1: TLabel;
+    RadioButton1: TRadioButton;
+    RadioButton2: TRadioButton;
+    procedure BitBtn1Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure RadioButton1Click(Sender: TObject);
+    procedure RadioButton2Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  value, total, proz: integer;
+  res_str, testkill, t1, evalue: string;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm2 }
+uses Unit2;
+
+{ TForm1 }
+
+//TaskKill
+procedure TaskKill(FileName: String);
+var AProcess: TProcess;
+  begin
+    AProcess := TProcess.Create(nil);
+    AProcess.CommandLine := 'cmd  /x/c taskkill /f /im "'+FileName+'"';
+    //AProcess.CommandLine := 'notepad.exe';
+    AProcess.Options := AProcess.Options + [poWaitOnExit];
+    AProcess.Execute;
+    AProcess.Free;
+    //ShowMessage ('notepad.exe остановлен!');
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  // открываем новое окно и -скрываем старое
+  Form2.Show;
+  Form1.Hide;
+  //Form1.Visible := false;
+
+  // Килл теста
+  if length(Form1.Edit4.Text) <> 0 then
+    begin
+    testkill := trim(Form1.Edit4.Text);
+    t1 := testkill[length(testkill)] +
+          testkill[length(testkill)-1] +
+          testkill[length(testkill)-2] +
+          testkill[length(testkill)-3];
+    // имя килла
+    if t1 = 'exe.' then
+      TaskKill(testkill)
+    else
+      TaskKill(testkill+'.exe');
+  end;
+
+  // меняем имя титула Form2
+  if length(Form1.Edit3.Text) <> 0 then
+    Form2.Caption := trim(Form1.Edit3.Text) + ' — результат тестирования easyQuizzy'
+  else
+    Form2.Caption := 'Created by @airmagicty — результат тестирования easyQuizzy';
+
+  // value total
+  if length(Form1.Edit1.Text) <> 0 then
+    value := StrToInt(Form1.Edit1.Text)
+  else value := 0;
+
+  if length(Form1.Edit2.Text) <> 0 then
+    total := StrToInt(Form1.Edit2.Text)
+  else total := 0;
+
+  // проверка какой тип теста
+  if Form1.RadioButton1.Checked = true then
+  begin
+    // большая цифра
+    Form2.Label1.Caption := Form1.Edit1.Text;
+
+    // label3 form2 балл
+    Form2.Label3.Caption := 'Подождите 10 секунд';
+
+    // окончание
+    if (value >= 11) and (value <= 14) then
+      res_str := 'баллов'
+    else case value mod 10 of
+      1: res_str := 'балл';
+      2..4: res_str := 'балла';
+      else res_str := 'баллов';
+    end;
+
+    // n из m
+    if (length(Form1.Edit1.Text) <> 0) and (length(Form1.Edit2.Text) <> 0) then
+      Form2.Label2.Caption := Form1.Edit1.Text + ' ' + res_str + ' из ' + Form1.Edit2.Text
+    else
+      Form2.Label2.Caption := 'Created by @airmagicty';
+
+  end
+  // если выбран 2й вариант
+  else if Form1.RadioButton2.Checked = true then
+  begin
+    // большая цифра
+    if total <> 0 then
+      proz := round((value/total)*100)
+    else
+      proz := 100;
+
+    if length(Form1.Edit5.Text) <= 0 then
+    begin
+      if proz = 0 then
+        evalue := '1';
+      if proz >= 10 then
+        evalue := '2';
+      if proz >= 50 then
+        evalue := '3';
+      if proz >= 70 then
+        evalue := '4';
+      if proz >= 90 then
+        evalue := '5';
+    end
+    else
+      evalue := Form1.Edit5.Text;
+
+    // label3 form2 балл
+    Form2.Label3.Caption := 'Подождите 30 секунд';
+
+    // окончание
+    if (value >= 11) and (value <= 14) then
+      res_str := 'правильных ответов'
+    else case value mod 10 of
+      1: res_str := 'правильный ответ';
+      2..4: res_str := 'правильных ответа';
+      else res_str := 'правильных ответов';
+    end;
+
+    // n из m
+    if (length(Form1.Edit1.Text) <> 0) and (length(Form1.Edit2.Text) <> 0) then
+    begin
+      Form2.Label2.Caption := Form1.Edit1.Text + ' ' + res_str + ' из ' + Form1.Edit2.Text;
+      Form2.Label1.Caption := evalue;
+    end
+    else
+      Form2.Label2.Caption := 'Created by @airmagicty';
+  end;
+end;
+
+procedure TForm1.RadioButton1Click(Sender: TObject);
+begin
+  Form1.Edit5.Visible := false;
+  Form1.RadioButton2.Caption := 'Экзамен';
+end;
+
+procedure TForm1.RadioButton2Click(Sender: TObject);
+begin
+  Form1.RadioButton2.Caption := 'Экз';
+  Form1.Edit5.Visible := true;
+end;
+
+procedure TForm1.BitBtn1Click(Sender: TObject);
+begin
+  ShowMessage('FLTest - Генератор результата тестирования easyQuizzy'+#13+
+              'Версия: 1.0 release'+#13+
+              'Разработчик: airmagicty'+#13+
+              '© 2022г'+#13+
+              #13+
+              'Инструкция по использованию:'+#13+
+              '1. В первую строку введите [количество полученных баллов] из [всего баллов (обычно равно количеству вопросов)].'+#13+
+              '2. Во вторую строку [Заголовок] введите текст, который будет оборажаться сверху при сгенерированном окне тестирования. К примеру, мой тест имеет заголовок «Проверочный тест к главе 1 "Информация и информационные процессы"».'+#13+
+              '3. В третью строку [Процесс.exe] введите имя файла тестирования, который вы запустили. Например мой файл называется "test-10-1.exe" и я введу "test-10-1" (можно и с .exe, программа все-равно поймет правильно). Это необходимо для того, чтобы завершить уже запущенное тестирование (по умолчанию у них защита от закрытия крестиком). Поле можно оставить пустым, если такого процесса нет, и оно будет прогнорировано.'+#13+
+              '4. Выберите тип тестирования: простое или экзаменационное. Разница в таймере ожидания, "баллы" заменены на "ответы" и изменен формат оценки.'+#13+
+              '4.1 При выборе экзаменационного типы вы можете вручную задать желаемую оценку. Игнорируйте это поле, если хотите, чтобы рассчет произвелся автоматически (90-70-50-1-0).'+#13+
+              '5. Нажмите [Создать] - это окно скроется, закроется открытый процесс (если поле [Процесс.exe] не пустое) и откроется сгенерированное окно с заданными параметрами.'+#13+
+              'Примечание1: при сворачивании сгенерированного окна с результатом - оно скроется.'+#13+
+              'Примечание2: с помощью кнопки [Сохранить в файл...] можно загрузить заранее подготовленный вами отчет (только .rtf).'+#13+
+              #13+
+              'Создано для: Босова Л. Л. - Электронное приложение к учебнику «Информатика» + Экзамен на easyQuizzy'+#13);
+end;
+
+end.
+
